b20662f8054d8d12470e8350d81c68c92c2d61b2
Daniil <zelenko.danil@gmail.com>
2023-12-29 15:45:10 +0100

    logger init

---
+++ src/tools/logger/logger.filter.ts
@@ -0,0 +1,24 @@
+import {
+  Catch,
+  ArgumentsHost,
+  HttpServer,
+  HttpException,
+} from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { LoggerService } from './logger.service';
+
+@Catch()
+export class LoggerFilter extends BaseExceptionFilter {
+  constructor(
+    private readonly loggerService: LoggerService,
+    httpSever: HttpServer,
+  ) {
+    super(httpSever);
+  }
+  catch(exception: unknown, host: ArgumentsHost) {
+    if (exception instanceof HttpException) {
+      this.loggerService.error(exception.message, '', exception.name);
+    }
+    super.catch(exception, host);
+  }
+}
