2339eb06a0fa3ca8c73e802a3f0c7d8357461628
Daniil <zelenko.danil@gmail.com>
2024-01-25 16:57:14 +0100

    restruct games

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import * as bcrypt from 'bcrypt';
 import { CreateUserDto, UpdateUserDto } from './dto';
 import { UserModel } from './model';
 import { AuthService, Tokens } from '../auth';
+import { UserGames } from './entities';
 
 @Injectable()
 export class UserService {
@@ -35,8 +36,49 @@ export class UserService {
     return this.model.findProfile(id);
   }
 
+  private restructUserGames(games: UserGames[]) {
+    return games.map((g) => {
+      const time = Math.floor(g.maxTime / (1000 * 60));
+      const inc = Math.floor(g.timeIncrement / 1000);
+      const [pl1, pl2] = g.players.map((pl) => {
+        return {
+          userId: pl.userId,
+          side: pl.side,
+          winner: pl.isWinner,
+          name: pl.user.name,
+        };
+      });
+      const result = g.isDraw
+        ? { pl1, pl2 }
+        : pl1.winner
+        ? { winner: pl1, looser: pl2 }
+        : { winner: pl2, looser: pl1 };
+      return {
+        id: g.id,
+        key: g.id,
+        cnf: {
+          inc,
+          time,
+        },
+        result,
+        sidepick: g.sideSelecting,
+      };
+    });
+  }
   public async games(id: number) {
-    return { games: await this.model.findUserGames(id) };
+    const gms = await this.model.findUserGames(id);
+    const parsed = this.restructUserGames(gms);
+    let wins = 0;
+    let looses = 0;
+    let draws = 0;
+    for (const game of parsed) {
+      const r = game.result;
+      if (r.winner) {
+        if (r.winner.userId === id) wins++;
+        else looses++;
+      } else draws++;
+    }
+    return { games: parsed, wins, looses, draws };
   }
 
   public update(id: number, updateUserDto: UpdateUserDto) {
